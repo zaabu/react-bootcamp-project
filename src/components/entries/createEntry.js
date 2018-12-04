191da8c79962e20fdc051cff15c0bfe72ce23501
Muhumuza Brian <muhumuzabr@gmail.com>
2018-12-04 10:19:25 +0300

    Add Heroku generated app.json

---
+++ src/components/entries/createEntry.js
@@ -0,0 +1,79 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import CreateEntryView   from "../../views/entries/createEntry";
+import { createDiaryEntry } from "../../actions/entryActions/entryAction";
+
+
+class CreateDiaryEntryComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false,
+            title: "",
+            body: ""
+        };
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    };
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.body
+        };
+        this.props.dispatch(createDiaryEntry(data));
+
+    };
+    modalClose = () => {
+        this.toggle();
+        this.handleSubmit();
+    }
+
+    render() {
+        const { modal, title, body} = this.state;
+        const classname = this.props.className;
+        return (
+            <div>
+                <CreateEntryView
+                    toggle={this.toggle}
+                    modal={modal}
+                    title={title}
+                    body={body}
+                    classname={classname}
+                    closemodal={this.modalClose}
+                    handleChange={this.handleChange}
+                />
+            
+            </div>
+        )
+    }
+}
+
+CreateDiaryEntryComponent.propTypes = {
+    className: PropTypes.string,
+    dispatch: PropTypes.func
+};
+
+const mapDispatchToProps = dispatch => ({ dispatch });
+const mapStateToProps = state => ({
+    entry: state.entriesReducer
+
+});
+
+//import this Component
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+
+)(CreateDiaryEntryComponent);
--- src/components/entries/viewEntries.js
+++ src/components/entries/viewEntries.js
@@ -3,6 +3,9 @@ import Viewallentries from "../../views/entries/ViewEntries";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { retrieveAllEntries } from "../../actions/entryActions/entryAction";
+import CreateDiaryEntryComponent from "../../components/entries/createEntry";
+
+
 
 export class ViewEntries extends Component {
   constructor(props) {
@@ -19,7 +22,15 @@ export class ViewEntries extends Component {
     
     return (
       <div>
-        <h1 class="title">Diaries</h1>
+        <h1 class="title d-inline">DIARY</h1>
+       
+        
+        <CreateDiaryEntryComponent />
+        {/* <div class="button_cont d-inline user-button example_a" > */}
+        {/* <div class="button_cont d-inline" ><a class="example_a" href="add-website-here" target="_blank" rel="nofollow noopener">Log Out</a></div> */}
+        
+        {/* <h2 class="d-inline pencil-icon"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGLSURBVGhD7dq7SgNRGEXhUUGtFAuvvYKKWImthfgkIpjW0kpsBCvfQMRKFPFeamVlYeOttlBB0EYELXTtwIFhmInOJJL/xLPhKxJSnJVAEjKJwsJyrRnzuMQnXrGFUXizVmzjK8UbpuHFRqBXIC1EntEJLzaBSjGzMLl27OAe7tmuFLMGc1PECXTAB3TALStmBaYWj3jEMJJLi5mBmbXhpwi3eMwpmmBieSLcFHOO7vItA8sT0YUlDJZvGVreiAvosau6w8oUcYy8EXfoh4lVEzEAEysacYsQUcvpW2yIsLB/H2HmLVYRRwgR9V7RiBuEiFquYSIO4SL0y0fWQsRfrpqIPpiYIg4QIuq9ohHXCBG1XMNE7CNE1HvVRPTCxBSxB68jNHe5y+uIIehgHxjTHRmLR1zBVIRWgg4nG2hBcuYjtF24kLQYLyJ04BfEQ+IxXkRok0hGOJvwIkJbRDIgSRE9ML0zpB1enrAO8xHaO9zB9RcJXVFawDjMXHT8zeawjCno0z0srPCi6BsHNBINm/BYCQAAAABJRU5ErkJggg=="></img></h2> */}
+
         <Viewallentries results={entries} />
       </div>
     );
